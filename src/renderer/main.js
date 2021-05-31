e474c79b3e7d941974fe49f75ed084e8e040d0ac
Roman <romanranniew@gmail.com>
2021-05-31 18:33:35 +0200

    Update packages

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -7,13 +7,6 @@ import store from './store'
 import Hammer from 'hammerjs'
 import Materialize from 'materialize-css'
 
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-global.deb = function() {
-    if (process.env.production) {
-        return 0;
-    }
-    console.log.apply(console, arguments)
-}
 
 Vue.config.productionTip = false
 /* eslint-disable no-new */
